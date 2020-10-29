099de1f4cef60b714e50858fc10dc8229a55514d
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-10-29 21:36:43 +0100

    Feature/mast 3 create login register page (#3)
    * Login page and registration page UI have been added.
    
    * Comment resolution
    
    Co-authored-by: mkyjovsky <michal.kyjovsky@getmanta.com>
    Co-authored-by: root <root@mkyjovsky01.localdomain>

--- mast/app.py
+++ mast/app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+@app.route('/login')
+def login():
     return render_template('login.html')
 
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
