6fae7b0a1f13de702f3a40e3eecc45c5a93fd182
drozt <33603625+TomasDrozdik@users.noreply.github.com>
2020-10-30 16:45:40 +0100

    Feature/mast 11 (#4)
    * Create wtf forms
    
    * Add CSRF protection
    
    * Add form validation
    
    * Add logging
    
    * Resolve PR comments
    
    * Unify auth-template includes, call logger automatically.
    
    * Package architecture, uWSGI ini, error logging, database interface provided, venv specification created
    
    * Cleanup after merge
    
    Co-authored-by: mkyjovsky <michal.kyjovsky@getmanta.com>

--- mast/forms.py
+++ mast/forms.py
@@ -1,1 +1,39 @@
-#ADD FLASK
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+import logging
+
+## Logging setup
+form_logger = logging.getLogger('form_submission')
+
+def log_form_submit(form):
+    form_logger.info(f'Received {type(form)}')
+    has_error = False
+    for field in form:
+        if field.errors:
+            has_error = True
+            form_logger.error(f'FIELD[{field.label.text}] - ERRORS{field.errors} - VALUE["{field.data}"]')
+    if not has_error:
+        form_logger.info('Form OK')
+    else:
+        form_logger.error('From contained Errors!')
+
+class LoggingFlaskForm(FlaskForm):
+    def validate(self):
+        res = super().validate()
+        log_form_submit(self)
+        return res
+
+class RegisterForm(LoggingFlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=50)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Create and account')
+
+
+class LoginForm(LoggingFlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
---
+++ mast/views.py
@@ -0,0 +1,33 @@
+from flask import redirect, request, render_template, url_for
+from mast.forms import LoginForm, RegisterForm
+from mast import app
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        form = LoginForm()
+        return render_template('login.html', form=form)
+    else:
+        form = LoginForm(request.form)
+        if form.validate():
+            # TODO: redirect the user to main page
+            return 'MAST homepage'
+        else:
+            return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render_template('register.html', form=form)
+    else:
+        form = RegisterForm(request.form)
+        if form.validate():
+            # TODO: add user to database
+            return redirect(url_for('login'))
+        else:
+            return render_template('register.html', form=form)
+
+
