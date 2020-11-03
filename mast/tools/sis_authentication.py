e292b9bd40262a0cde1a1aed3aa284a6bc8376e4
KleprlikJan <kleprlikjan@gmail.com>
2020-11-03 15:48:27 +0100

    Reformat file

--- mast/tools/sis_authentication.py
+++ mast/tools/sis_authentication.py
@@ -16,72 +16,96 @@ def authenticate_via_sis(name, surname, login=None, ukco=None, is_employee=False
     :return: True if such person exists, False otherwise
     """
     if is_employee:
-        if name == None or surname == None:
+        if not name or not surname:
             # expecting both name and surname
             print('Expected both name and surname\nName={}\nSurname={}'.format(name, surname))
             return False
-        return authenticate_employee(name=name, surname=surname)
+        return __authenticate_employee(name=name, surname=surname)
     else:
-        if name == None or surname == None:
+        if not name or not surname:
             # expecting both name and surname
             print('Expected both name and surname\nName = {}\nSurname = {}'.format(name, surname))
             return False
-        if login == None and ukco == None:
+        if not login and not ukco:
             # expecting at least one of the following login, ukco
             print('Expected at least one of login, ukco\nLogin = {}\nUKCO = {}'.format(login, ukco))
             return False
-        return authenticate_student(name=name, surname=surname, login=login, ukco=ukco)
+        return __authenticate_student(name=name, surname=surname, login=login, ukco=ukco)
 
 
-def authenticate_student(name, surname, login=None, ukco=None):
-    url = build_url(is_employee=False, name=name, surname=surname, login=login, ukco=ukco)
+def __authenticate_student(name, surname, login=None, ukco=None):
+    """
+    Checks if such student exits when asked at is.cuni.cz/studium/kdojekdo
+    :param name: name
+    :param surname: surname
+    :param login: login
+    :param ukco: UKČO
+    :return: True if such person exists, False otherwise
+    """
+    url = __build_url(is_employee=False, name=name, surname=surname, login=login, ukco=ukco)
     page = requests.get(url)
-    nubmer_of_results = get_number_of_students(page=page, name=name, surname=surname, login=login, ukco=ukco)
+    nubmer_of_results = __get_number_of_students(page=page)
 
     if int(nubmer_of_results) >= 1:
         return True
     return False
 
 
-def authenticate_employee(name, surname):
-    url = build_url(is_employee=True, name=name, surname=surname)
+def __authenticate_employee(name, surname):
+    """
+    Checks if such employee exits when asked at is.cuni.cz/studium/kdojekdo
+    :param name: name
+    :param surname: surname
+    :return: True if such person exists, False otherwise
+    """
+    url = __build_url(is_employee=True, name=name, surname=surname)
     page = requests.get(url)
-    nubmer_of_results = get_number_of_employees(page=page, name=name, surname=surname)
+    nubmer_of_results = __get_number_of_employees(page=page)
 
     if int(nubmer_of_results) >= 1:
         return True
     return False
 
 
-def get_number_of_employees(page, name, surname):
+def __get_number_of_employees(page):
+    """
+    Searches for number of results at queried page
+    :param page: queried page with results
+    :return: number of employees in the results
+    """
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.select('#page_div > b:nth-child(3)')
     for text in content:
         for part in text:
-            return get_number(part)
+            return __get_number(part)
     return 0
 
 
-def get_number_of_students(page, name, surname, login=None, ukco=None):
+def __get_number_of_students(page):
+    """
+    Searches for number of results at queried page
+    :param page: queried page with results
+    :return: number of students in the results
+    """
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.select('#content > table >  tr > td.info_text > ul > li')
     for text in content:
         for part in text:
             if ("Number of students who do not have the right to seek:" in part) or \
                     ("Počet studentů, které nemáte právo vyhledat:" in part):
-                return get_number(part)
+                return __get_number(part)
     return 0
 
 
-def get_number(text):
+def __get_number(text):
     return int(re.search(r'\d+', text).group())
 
 
-def build_url(is_employee, name=None, surname=None, login=None, ukco=None):
+def __build_url(is_employee, name=None, surname=None, login=None, ukco=None):
     # dummy inicialization
     url = ''
 
-    if (is_employee):
+    if is_employee:
         url = 'https://is.cuni.cz/studium/kdojekdo/index.php?do=hledani&koho=u&'
         url += 'fakulta=&'                  # change if we want to give access to all employees not just MFF
         url += 'prijmeni={}&'.format(surname)
