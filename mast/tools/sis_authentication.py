0700e0d3a721f65ba29df288b42ce7195fa9a04f
KleprlikJan <kleprlikjan@gmail.com>
2020-11-03 13:03:29 +0100

    implement employee authentication

--- mast/tools/sis_authentication.py
+++ mast/tools/sis_authentication.py
@@ -3,11 +3,23 @@ from bs4 import BeautifulSoup
 import re
 
 
-def authenticate_via_sis(name, surname, login=None, ukco=None, is_employee = False):
+def authenticate_via_sis(name, surname, login=None, ukco=None, is_employee=False):
     if is_employee:
-        authenticate_employee(name, surname)
+        if name == None or surname == None:
+            # expecting both name and surname
+            print('Expected both name and surname\nName={}\nSurname={}'.format(name, surname))
+            return False
+        return authenticate_employee(name=name, surname=surname)
     else:
-        authenticate_student(name=name, surname=surname, login=login, ukco=ukco)
+        if name == None or surname == None:
+            # expecting both name and surname
+            print('Expected both name and surname\nName = {}\nSurname = {}'.format(name, surname))
+            return False
+        if login == None and ukco == None:
+            # expecting at least one of the following login, ukco
+            print('Expected at least one of login, ukco\nLogin = {}\nUKCO = {}'.format(login, ukco))
+            return False
+        return authenticate_student(name=name, surname=surname, login=login, ukco=ukco)
 
 
 def authenticate_student(name, surname, login=None, ukco=None):
@@ -21,10 +33,25 @@ def authenticate_student(name, surname, login=None, ukco=None):
 
 
 def authenticate_employee(name, surname):
-    ulr = build_url(is_employee=True, name=name, surname=surname)
+    url = build_url(is_employee=True, name=name, surname=surname)
+    page = requests.get(url)
+    nubmer_of_results = get_number_of_employees(page=page, name=name, surname=surname)
+
+    if int(nubmer_of_results) >= 1:
+        return True
+    return False
 
 
-def get_number_of_students(page, name=None, surname=None, login=None, ukco=None):
+def get_number_of_employees(page, name, surname):
+    soup = BeautifulSoup(page.content, 'html.parser')
+    content = soup.select('#page_div > b:nth-child(3)')
+    for text in content:
+        for part in text:
+            return get_number(part)
+    return 0
+
+
+def get_number_of_students(page, name, surname, login=None, ukco=None):
     soup = BeautifulSoup(page.content, 'html.parser')
     content = soup.select('#content > table >  tr > td.info_text > ul > li')
     for text in content:
@@ -44,21 +71,12 @@ def build_url(is_employee, name=None, surname=None, login=None, ukco=None):
     url = ''
 
     if (is_employee):
-        if name == None or surname == None:
-            # expecting both name and surname
-            return 'Expected both name and surname\nName={}\nSurname={}'.format(name, surname)
         url = 'https://is.cuni.cz/studium/kdojekdo/index.php?do=hledani&koho=u&'
         url += 'fakulta=&'                  # change if we want to give access to all employees not just MFF
         url += 'prijmeni={}&'.format(surname)
         url += 'jmeno={}&'.format(name)
         url += 'inukat=0&exukat=0&neuci=0&pocet=50&vyhledej=Vyhledej'
     else:
-        if name == None or surname == None:
-            # expecting both name and surname
-            return 'Expected both name and surname\nName = {}\nSurname = {}'.format(name, surname)
-        if login == None and ukco == None:
-            # expecting at least one of the following login, ukco
-            return 'Expected at least one of login, ukco\nLogin = {}\nUKCO = {}'.format(login,ukco)
         url = 'https://is.cuni.cz/studium/kdojekdo/index.php?do=hledani&koho=s&'
         url += 'fakulta=11320&'             # change if we want to give access to all students not just MFF
         url += 'prijmeni={}&'.format(surname)
@@ -70,13 +88,4 @@ def build_url(is_employee, name=None, surname=None, login=None, ukco=None):
     return url
 
 if __name__ == '__main__':
-    name = 'Jan'
-    surname = 'Kleprlik'
-    login = 'kleprlij'
-    ukco = '44214998'
-    print(authenticate_student(name=name, surname=surname, login=login, ukco=ukco))
-
-
-    name = 'Pavel'
-    surname = 'Jezek'
-    print(authenticate_employee(name=name, surname=surname))
+    pass
