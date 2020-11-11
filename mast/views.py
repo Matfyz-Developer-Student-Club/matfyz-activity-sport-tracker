304cd301926da711ca0c5855a9c73e10ebc693b8
Dúšis <47784885+spiduso@users.noreply.github.com>
2020-11-11 15:51:17 +0100

    new home/login/register (#20)
    * new home/login/register
    
    * deleted home button, home in navbar, smaller pictures
    
    * code adjustments
    
    * removed jquery

--- mast/views.py
+++ mast/views.py
@@ -31,7 +31,8 @@ def login():
                 login_user(user, remember=form.remember.data)
                 return redirect(url_for('home'))
             else:
-                form.email.errors.append('Specified pair of email and password is invalid!')
+                form.email.errors.append(
+                    'Specified pair of email and password is invalid!')
     return render_template('login.html', form=form)
 
 
@@ -45,8 +46,10 @@ def register():
     else:
         form = RegisterForm(request.form)
         if form.validate_on_submit():
-            hashed_password = bcr.generate_password_hash(form.password.data.strip()).decode('UTF-8')
-            user = User(email=form.email.data.lower().strip(), password=hashed_password)
+            hashed_password = bcr.generate_password_hash(
+                form.password.data.strip()).decode('UTF-8')
+            user = User(email=form.email.data.lower().strip(),
+                        password=hashed_password)
             login_user(user)
             return redirect(url_for('home'))
         else:
@@ -70,7 +73,8 @@ def home():
     if add_activity_form.validate_on_submit():
         filename = secure_filename(add_activity_form.file.data.filename)
         path = os.path.join(__file__, os.pardir)
-        add_activity_form.file.data.save(os.path.join(os.path.abspath(path), UPLOAD_FILE_DIR, filename))
+        add_activity_form.file.data.save(os.path.join(
+            os.path.abspath(path), UPLOAD_FILE_DIR, filename))
         a_type = ActivityType
         if add_activity_form.activity.data == ActivityType.Ride.name:
             a_type = ActivityType.Ride
@@ -84,7 +88,8 @@ def home():
         seconds = activity[0][1].total_seconds()
         avg_seconds = round(seconds / activity[0][0])
         full_time = (datetime.datetime(2000, 1, 1, 0) + activity[0][1]).time()
-        avg_time = (datetime.datetime(2000, 1, 1, 0) + datetime.timedelta(seconds=avg_seconds)).time()
+        avg_time = (datetime.datetime(2000, 1, 1, 0) +
+                    datetime.timedelta(seconds=avg_seconds)).time()
         new_activity = Activity(datetime=activity[0][2], distance=activity[0][0], duration=full_time,
                                 average_duration_per_km=avg_time, type=a_type)
         session.save_new_user_activities(current_user.id, new_activity)
@@ -98,7 +103,8 @@ def home():
 @login_required
 def get_personal_stats():
     session = mast.queries.Queries()
-    data = session.get_total_distances_by_user_in_last_days(user_id=current_user.id, days=7)
+    data = session.get_total_distances_by_user_in_last_days(
+        user_id=current_user.id, days=7)
     labels = [key for key, val in data.items()]
     data = [val for key, val in data.items()]
     return jsonify({'payload': json.dumps({'data': data, 'labels': labels})})
@@ -114,8 +120,10 @@ def matfyz_challenges():
 @login_required
 def get_global_contest():
     session = mast.queries.Queries()
-    labels = ["Where we gonna make it by bike.", "Where we gonna make it on foot."]
-    data = [session.get_global_total_distance_on_bike(), session.get_global_total_distance_on_foot()]
+    labels = ["Where we gonna make it by bike.",
+              "Where we gonna make it on foot."]
+    data = [session.get_global_total_distance_on_bike(
+    ), session.get_global_total_distance_on_foot()]
     checkpoints = session.get_challenge_parts()
     return jsonify({'payload': json.dumps({'data': data, 'labels': labels, 'checkpoints': checkpoints})})
 
@@ -124,11 +132,16 @@ def get_global_contest():
 @login_required
 def running_5_km():
     session = mast.queries.Queries()
-    user_five = session.get_best_run_activities_by_user(current_user.id, Competition.Run5km, 10)
-    five_runner_men_under = session.get_top_users_best_run(Competition.Run5km, Sex.Male, Age.Under35, 10)
-    five_runner_men_above = session.get_top_users_best_run(Competition.Run5km, Sex.Male, Age.Over35, 10)
-    five_runner_women_under = session.get_top_users_best_run(Competition.Run5km, Sex.Female, Age.Under35, 10)
-    five_runner_women_above = session.get_top_users_best_run(Competition.Run5km, Sex.Female, Age.Over35, 10)
+    user_five = session.get_best_run_activities_by_user(
+        current_user.id, Competition.Run5km, 10)
+    five_runner_men_under = session.get_top_users_best_run(
+        Competition.Run5km, Sex.Male, Age.Under35, 10)
+    five_runner_men_above = session.get_top_users_best_run(
+        Competition.Run5km, Sex.Male, Age.Over35, 10)
+    five_runner_women_under = session.get_top_users_best_run(
+        Competition.Run5km, Sex.Female, Age.Under35, 10)
+    five_runner_women_above = session.get_top_users_best_run(
+        Competition.Run5km, Sex.Female, Age.Over35, 10)
 
     return render_template("running_5_km.html", title="Running-5", user_five=user_five,
                            five_runner_men_above=five_runner_men_above, five_runner_men_under=five_runner_men_under,
@@ -140,11 +153,16 @@ def running_5_km():
 @login_required
 def running_10_km():
     session = mast.queries.Queries()
-    user_ten = session.get_best_run_activities_by_user(current_user.id, Competition.Run10km, 10)
-    ten_runner_men_under = session.get_top_users_best_run(Competition.Run10km, Sex.Male, Age.Under35, 10)
-    ten_runner_men_above = session.get_top_users_best_run(Competition.Run10km, Sex.Male, Age.Over35, 10)
-    ten_runner_women_under = session.get_top_users_best_run(Competition.Run10km, Sex.Female, Age.Under35, 10)
-    ten_runner_women_above = session.get_top_users_best_run(Competition.Run10km, Sex.Female, Age.Over35, 10)
+    user_ten = session.get_best_run_activities_by_user(
+        current_user.id, Competition.Run10km, 10)
+    ten_runner_men_under = session.get_top_users_best_run(
+        Competition.Run10km, Sex.Male, Age.Under35, 10)
+    ten_runner_men_above = session.get_top_users_best_run(
+        Competition.Run10km, Sex.Male, Age.Over35, 10)
+    ten_runner_women_under = session.get_top_users_best_run(
+        Competition.Run10km, Sex.Female, Age.Under35, 10)
+    ten_runner_women_above = session.get_top_users_best_run(
+        Competition.Run10km, Sex.Female, Age.Over35, 10)
 
     return render_template("running_10_km.html", title="Running-10", user_ten=user_ten,
                            ten_runner_men_above=ten_runner_men_above, ten_runner_men_under=ten_runner_men_under,
@@ -156,7 +174,8 @@ def running_10_km():
 def running_walking():
     session = mast.queries.Queries()
     jogging_global = session.get_top_users_total_distance_on_foot(10)
-    jogging_personal = session.get_user_last_activities_on_foot(current_user.id, 10)
+    jogging_personal = session.get_user_last_activities_on_foot(
+        current_user.id, 10)
 
     jogging_personal = jogging_personal if jogging_personal else []
     jogging_global = jogging_global if jogging_global else []
@@ -196,8 +215,10 @@ def user_settings():
         elif request.form['submit'] == 'Change password':
             change_password_form = ChangePasswordForm(request.form)
             if change_password_form.validate():
-                hashed_password = bcr.generate_password_hash(change_password_form.password.data).decode('UTF-8')
-                db.session.query(User).filter(User.id == current_user.id).update({User.password: hashed_password})
+                hashed_password = bcr.generate_password_hash(
+                    change_password_form.password.data).decode('UTF-8')
+                db.session.query(User).filter(User.id == current_user.id).update(
+                    {User.password: hashed_password})
                 db.session.commit()
                 return redirect(url_for('user_settings'))
             else:
@@ -228,7 +249,8 @@ def user_settings():
 def cycling():
     session = mast.queries.Queries()
     cyclists_global = session.get_top_users_total_distance_on_bike(10)
-    cyclist_personal = session.get_user_last_activities_on_bike(current_user.id, 10)
+    cyclist_personal = session.get_user_last_activities_on_bike(
+        current_user.id, 10)
 
     cyclist_personal = cyclist_personal if cyclist_personal else []
     cyclists_global = cyclists_global if cyclists_global else []
